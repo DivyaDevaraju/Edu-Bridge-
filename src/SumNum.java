8ad908a1ea496c6b833e1517b2e7c8bae293a398
divya <divyagmail.com>
2024-11-06 15:34:56 +0530

    commit

---
+++ src/SumNum.java
@@ -0,0 +1,15 @@
+
+public class SumNum {
+
+	public static void main(String[] args) {
+		int num=1,sum=0;
+		while(num<=100)
+		{
+			sum=sum+num;
+			num++;
+		}
+		
+		System.out.println("Sum is:"+sum);
+	}
+
+}
